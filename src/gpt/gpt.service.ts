edfdccbc76d75ec9383b92b756c2792b78e7d22d
edson.rosales <edson.rosales@Edsons-Macbook-Pro-Job.local>
2024-05-14 16:41:30 -0600

    feat[DTOs]: Pass arguments from DTO to the use case

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
   // Only call use Cases in this Service
-  async orthographyCheck() {
-    return await orthographyCheckUseCase();
+  async orthographyCheck(orthographyDto: OrthographyDto) {
+    return await orthographyCheckUseCase({
+      prompt: orthographyDto.prompt,
+    });
   }
 }
