e79e1b10c0d245abe8362a04baea8d5866db903b
edson.rosales <edson.rosales@Edsons-Macbook-Pro-Job.local>
2024-05-23 11:55:29 -0600

    feat[pros-cons-discusser-streams]: Modified the original version of pros-cons-discusser comparator controller to emit a chunk of strings for the endpoint response

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,4 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+
 import { GptService } from './gpt.service';
 import { OrthographyDto, ProsConsDiscusserDto } from './dtos';
 
@@ -14,5 +16,25 @@ export class GptController {
   @Post('pros-cons-discusser')
   prosConsDiscusser(@Body() prosConsDiscusserDto: ProsConsDiscusserDto) {
     return this.gptService.prosConsDiscusser(prosConsDiscusserDto);
+  }
+
+  @Post('pros-cons-discusser-stream')
+  async prosConsDiscusserStream(
+    @Body() prosConsDiscusserDto: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+    const stream =
+      await this.gptService.prosConsDiscusserStream(prosConsDiscusserDto);
+
+    // I need to emit the response based on nes @Res()
+    res.setHeader('Content-Type', 'application/json');
+    res.status(HttpStatus.OK);
+
+    // Use 'for await' to emit the response 'cause isn't a promise, is a combo emissions of strings
+    for await (const chunk of stream) {
+      const piece = chunk.choices[0].delta.content || '';
+      res.write(piece);
+    }
+    res.end(); // <--- Close the response
   }
 }
