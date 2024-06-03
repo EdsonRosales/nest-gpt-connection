bf724697cace6ff44d2197082ae2348c4d6f1933
edson.rosales <edson.rosales@192.168.1.6>
2024-06-03 16:27:10 -0600

    feat[AudioToText]: Created the use case to generate the transcriptions & subtitles

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -6,6 +6,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import OpenAI from 'openai';
 
 import {
+  audioToTextUseCase,
   orthographyCheckUseCase,
   prosConsDiscusserStreamUseCase,
   prosConsDiscusserUseCase,
@@ -61,5 +62,9 @@ export class GptService {
     if (!wasFound) throw new NotFoundException(`File ${fileId} not found`);
 
     return filePath;
+  }
+
+  async audioToText(audioFile: Express.Multer.File, prompt?: string) {
+    return await audioToTextUseCase(this.openai, { audioFile, prompt });
   }
 }
