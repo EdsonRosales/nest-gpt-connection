ee996d41d41e07c73ea012faf5f791d2995099e6
edson.rosales <edson.rosales@192.168.1.8>
2024-06-04 14:29:22 -0600

    feat[ImageGenerator]: Get the generated image based in the prompt created by Dall-e

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -11,5 +11,23 @@ export const imageGenerationUseCase = async (
   options: Options,
 ) => {
   const { prompt, maskImage, originalImage } = options;
-  console.log({ prompt, maskImage, originalImage });
+  // TO DO: Verify the original image
+
+  const resp = await openai.images.generate({
+    prompt,
+    model: 'dall-e-3',
+    n: 1,
+    size: '1024x1024',
+    quality: 'standard',
+    response_format: 'url',
+  });
+
+  // TO DO: Save the image in FileSystem
+
+  console.log(resp);
+  return {
+    url: resp.data[0].url,
+    localPath: '',
+    revised_promp: resp.data[0].revised_prompt,
+  };
 };
