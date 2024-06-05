dcbc1efeb7d7cb54820ec7d3ecbd2f7befbeef7f
edson.rosales <edson.rosales@192.168.1.8>
2024-06-04 18:19:47 -0600

    feat[ImageGenerator]: Edit images & functios to create an editable image mask for OpenAI

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import OpenAI from 'openai';
-import { downloadImageAsPng } from '../../helpers';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from '../../helpers';
 
 interface Options {
   prompt: string;
@@ -11,24 +14,49 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   options: Options,
 ) => {
-  const { prompt } = options;
-  // TO DO: Verify the original image
+  const { prompt, originalImage, maskImage } = options;
+  // Verify the original image
+  if (!originalImage || !maskImage) {
+    const resp = await openai.images.generate({
+      prompt,
+      model: 'dall-e-3',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
 
-  const resp = await openai.images.generate({
+    // Save the image in FileSystem
+    const url = await downloadImageAsPng(resp.data[0].url);
+
+    return {
+      url, // <--- TO DO: http://localhost:3000/image-generator/nameFile.png
+      openAIUrl: resp.data[0].url,
+      revised_promp: resp.data[0].revised_prompt,
+    };
+  }
+
+  // The original Image source
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
+    model: 'dall-e-2',
     prompt,
-    model: 'dall-e-3',
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
-  // TO DO: Save the image in FileSystem
-  const url = await downloadImageAsPng(resp.data[0].url);
+  const localImagePath = await downloadImageAsPng(response.data[0].url);
+  const fileName = path.basename(localImagePath);
+  const publicUrl = `localhost:3000/${fileName}`; // <--- Just a temporal solution
 
   return {
-    url,
-    openAIUrl: resp.data[0].url,
-    revised_promp: resp.data[0].revised_prompt,
+    url: publicUrl, // <--- TO DO: http://localhost:3000/image-generator/nameFile.png
+    openAIUrl: response.data[0].url,
+    revised_promp: response.data[0].revised_prompt,
   };
 };
