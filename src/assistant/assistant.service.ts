5487f9da89932424b37c19f502bba44512501276
Edson Rosales <edson.rosales@192.168.1.17>
2024-06-10 13:58:40 -0600

    feat[Assistants]: Create run (execution of thread use case)

--- src/assistant/assistant.service.ts
+++ src/assistant/assistant.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 import OpenAI from 'openai';
-import { createMessageUseCase, createThreadUseCase } from './use-cases';
+import {
+  createMessageUseCase,
+  createRunUseCase,
+  createThreadUseCase,
+} from './use-cases';
 import { QuestionDto } from './dtos/question.dto';
 @Injectable()
 export class AssistantService {
@@ -19,6 +23,7 @@ export class AssistantService {
       question,
       threadId,
     });
-    console.log(message);
+
+    const run = await createRunUseCase(this.openai, { threadId });
   }
 }
---
+++ src/assistant/use-cases/create-run.use-case.ts
@@ -0,0 +1,16 @@
+import OpenAI from 'openai';
+
+interface Options {
+  threadId: string;
+  assistantId?: string;
+}
+
+export const createRunUseCase = async (openai: OpenAI, options: Options) => {
+  const { threadId, assistantId = 'asst_aIgSicqjFYYv7GXYeyRZKmtb' } = options; // TO DO: Move assistantId to env
+
+  const run = await openai.beta.threads.runs.create(threadId, {
+    assistant_id: assistantId,
+    // instructions: ¡CAUTION! assistant_id can override the assistant_id set in the thread.
+  });
+  console.log({ run });
+};
